7156397993b75af7c914802c73e4958c308bec0e
tsporthd <tsporthd@gmail.com>
2018-01-01 17:28:10 -0500

    Add new components to repo

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,27 @@
 import React, {Component} from 'react';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './search_bar';
+import VideoList from './video_lists';
 
 const API_KEY = 'AIzaSyCyCiygukK1rDBBGHoPu3VDc2Mr3QLxPQs';
-
-import SearchBar from './search_bar';
+const youTubeSearch = 'surfing';
 
 
 export default class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {videos: []};
+        YTSearch({key: API_KEY, youTubeSearch},(videos) => {
+           this.setState( {videos});
+        });
+
+    }
+
     render() {
         return (
             <div>
                 <SearchBar/>
+                <VideoList videos={this.state.videos}/>
             </div>
         );
     }
